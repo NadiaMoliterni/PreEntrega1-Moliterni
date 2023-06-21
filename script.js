9bfe55a1a920d2a97a06559a4dba8f93818bf741
nadiamoliterni <nanu.moli@hotmail.com>
2023-06-21 15:33:46 -0300

    agregamos arrays obj ..

--- script.js
+++ script.js
@@ -1,36 +1,97 @@
-/* function pedirDato(texto) {
-    let dato = prompt(texto)
-    return dato
-}
-
-let nombre = pedirDato("Ingrese su nombre")
-let apellido = pedirDato("Ingrese su apellido")
-
-alert("Bienvenid@ " + nombre + " " + apellido)
- */
-
-//esto va a reemplazar la manera de arriba de pedir nombre y apellido
-/* const persona = {}
+const persona = {}
 
 persona.nombre = prompt("Ingrese su nombre")
 persona.apellido = prompt("Ingrese su apellido")
 console.log("Bienvenid@ " + persona.nombre + " " + persona.apellido)
 alert("Bienvenid@ " + persona.nombre + " " + persona.apellido)
- */
 
-/* 
+let productos = [
+    { id: 1, nombre: "mate argentina campeon 1", color: "azul", precio: 5000 },
+    { id: 2, nombre: "mate mundi", color: "blanco", precio: 3500 },
+    { id: 3, nombre: "mate moli borgoña", color: "rosa", precio: 2000 },
+    { id: 4, nombre: "mate moli", color: "blanco", precio: 2500 },
+    { id: 5, nombre: "mate argentina campeon 2", color: "negro", precio: 5000 },
+    { id: 6, nombre: "mate moli oliva", color: "verde", precio: 3200 },
+]
 
-let cantidadProductos = parseInt(prompt("Ingrese la cantidad de productos:"));
-let listaProductos = "";
+let carrito = []
+let seleccion = prompt("Hola! ¿Desea ver los productos para comprar?")
 
-for (let i = 0; i < cantidadProductos; i++) {
-    let producto = prompt("Ingrese el nombre del producto " + (i + 1));
-    let precio = parseFloat(prompt("Ingrese el precio del producto " + (i + 1)));
-
-    listaProductos += producto + " - $" + precio + "\n";
+if (seleccion === "si") {
+    alert("Lista de productos")
+    let todoslosProductos = productos.map(
+        (producto) => producto.nombre + " " + "$" + producto.precio
+    )
+    alert(todoslosProductos.join("\n"))
+} else if (seleccion === "no") {
+    alert("Gracias por su visita, hasta pronto!")
 }
 
-alert("Lista de productos:\n" + listaProductos);
+let busquedaP = prompt("Ingrese producto que desea buscar")
+const busqueda = productos.find((el) => el.nombre === busquedaP)
+
+if (busqueda) {
+    console.log(busqueda)
+    alert(busqueda.nombre + " " + "$" + busqueda.precio)
+}
+
+let filtrarP = prompt("Ingrese color que desea")
+const filtrar = productos.filter((el) => el.color === filtrarP)
+
+if (filtrar.length > 0) {
+    const nombresProductosFiltrados = filtrar.map((el) => el.nombre)
+    console.log(filtrar)
+    alert(`Los productos de color ${filtrarP} son: 
+    ${nombresProductosFiltrados.join(', ')}`)
+
+} else {
+    alert(`No hay productos de color ${filtrarP}`)
+}
+
+let total = 0
+
+while (seleccion !== "no") {
+    let producto = prompt("Agrega un producto al carrito")
+    let precio = 0
+
+    switch (producto) {
+        case "mate argentina campeon 1":
+            precio = 5000;
+            break;
+        case "mate mundi":
+            precio = 3500;
+            break;
+        case "mate moli borgoña":
+            precio = 2000;
+            break;
+        case "mate moli":
+            precio = 2500;
+            break;
+        case "mate argentina campeon 2":
+            precio = 5000;
+            break;
+        case "mate moli oliva":
+            precio = 3200;
+            break;
+        default:
+            alert("Por el momento no contamos con ese producto");
+            continue;
+    }
+
+    carrito.push({ producto, precio })
+    console.log(carrito)
+
+    seleccion = prompt("Desea seguir comprando?")
+
+    if (seleccion === "no") {
+        alert("Gracias por su visita!")
+        const listaCarrito = carrito.reduce((acc, el) => acc += `${el.producto} - $${el.precio} \n`, "")
+        alert("Productos que desea comprar: " + listaCarrito)
+    }
+    total = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
+
+}
+console.log(`Total: ${total}`)
 
 function calcularTotal(importe, iva) {
     return importe + importe * (iva / 100)
@@ -81,111 +142,18 @@ if (!isNaN(codigoPostal)) {
     console.log("Error")
     alert("El numero es incorrecto, por favor vuelva a ingresar su codigo postal")
 }
- */
-/* 
-ej cargar array con entrada
-const listaNombres = []
-let cantidad = 5
 
-do {
-    let entrada = prompt("Ingrese nombre del producto")
-    listaNombres.push(entrada.toUpperCase())
-    console.log(listaNombres.length)
-    
-} while (listaNombres.length != cantidad);
-
-const nuevaLista = listaNombres.concat[(ana)]
-alert(nuevaLista.join("\n")) */
-
-let productos = [
-    { id: 1, nombre: "Mate Argentina Campeon Az", precio: 5000 },
-    { id: 2, nombre: "Mate Mundi", precio: 3500 },
-    { id: 3, nombre: "Mate Moli Borgoña", precio: 2000 },
-    { id: 4, nombre: "Mate Moli Blanco", precio: 2500 },
-    { id: 5, nombre: "Mate Argentina Campeon Ng", precio: 5000 },
-    { id: 6, nombre: "Mate Moli Vd Oliva", precio: 3200 },
-]
-
-let carrito = []
-
-let seleccion = prompt("Hola! ¿Desea ver los productos para comprar?")
-
-// while (seleccion != "si" && seleccion != "no") {
-//     alert("Hola! ¿Desea ver los productos para comprar?")
-//     seleccion = prompt("Hola! ¿Desea ver los productos para comprar?")
-// }
-
-if (seleccion === "si") {
-    alert("lista de productos")
-    let todoslosProductos = productos.map(
-        (productos) => productos.nombre + " " + "$" + productos.precio
-    )
-    alert(todoslosProductos.join("\n"))
-} else if (seleccion === "no") {
-    alert("Gracias por su visita, hasta pronto!")
-}
-
-while (seleccion != "no") {
-    let producto = prompt("Agrega un producto al carrito")
-    let precio = 0
-
-    if (producto == "Mate Argentina Campeon Az" || "Mate Mundi" || "Mate Moli Borgoña" || "Mate Moli Blanco" || "Mate Argentina Campeon Ng" || "Mate Moli Vd Oliva") {
-        switch (productos) {
-            case "Mate Argentina Campeon Az":
-                precio = 5000
-                break;
-            case "Mate Mundi":
-                precio = 3500
-                break;
-            case "Mate Moli Borgoña":
-                precio = 2000
-                break;
-            case "Mate Moli Blanco":
-                precio = 2500
-                break;
-            case "Mate Argentina Campeon Ng":
-                precio = 5000
-                break;
-            case "Mate Moli Vd Oliva":
-                precio = 3200
-                break;
-            default:
-                break;
-        }
-        carrito.push({ producto, precio })
-        console.log(carrito)
-    } else {
-        alert("Por el momento no contamos con ese producto")
-    }
-    seleccion = prompt("Desea seguir comprando?")
-
-    while (seleccion === "no") {
-        alert("Gracias por su compra!")
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal.producto}, precio: ${carritoFinal.precio}`)
-        })
-        break;
-    }
-}
-
-const totalFinal = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
-console.log(totalFinal)
-
-
-
-/* 
 for (const producto of productos) {
-    console.log(producto.nombre)   
+    console.log(producto.nombre)
 }
 
 console.log(productos)
 
- */
-/* let productosFiltrados = productos.filter((producto) => producto.precio < 3000)
+let productosFiltrados = productos.filter((producto) => producto.precio < 3000)
 
 for (const producto of productosFiltrados) {
     console.log(producto.precio)
 }
- */
-let total = productos.reduce((acumulador, producto) => acumulador + producto.precio, 0)
+
+let totall = productos.reduce((acumulador, producto) => acumulador + producto.precio, 0)
 console.log(total)
